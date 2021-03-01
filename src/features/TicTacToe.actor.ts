406a32c03ac2f7ccb02d25b590d3a4e8147bda4b
MrNovado <novado@ymail.com>
2021-03-01 16:27:00 +0300

    basic machine declaration

---
+++ src/features/TicTacToe.actor.ts
@@ -0,0 +1,3 @@
+import { Machine } from 'xstate';
+
+export const TicTacToeActor = Machine({});
--- src/features/TicTacToe.machine.ts
+++ src/features/TicTacToe.machine.ts
@@ -1,1 +1,43 @@
-export const TicTacToeMachine = null;
+import { createMachine, DefaultContext } from 'xstate';
+
+type TicTacToeContext = DefaultContext;
+
+type TicTacToeEvents = { type: 'something' };
+
+const TicTacToeStateNodes = {
+  selectingOpponent: '@/selectingOpponent',
+  decidingWhosGoingFirst: '@/decidingWhosGoingFirst',
+  playing: '@/playing',
+  playingTakingTurn: '@/playing/takingTurn',
+  playingCheckingGameEnd: '@/playing/checkingGameEnd',
+  showingGameEndResults: '@/showingGameEndResults',
+} as const;
+
+// short-hand helper
+const S = TicTacToeStateNodes;
+
+type TicTacToeState = {
+  context: TicTacToeContext;
+  value:
+    | typeof S.selectingOpponent
+    | typeof S.decidingWhosGoingFirst
+    | typeof S.playing
+    | { playing: typeof S.playingTakingTurn }
+    | { playing: typeof S.playingCheckingGameEnd }
+    | typeof S.showingGameEndResults;
+};
+
+export const TicTacToeMachine = createMachine<TicTacToeContext, TicTacToeEvents, TicTacToeState>({
+  context: undefined,
+  states: {
+    [S.selectingOpponent]: {},
+    [S.decidingWhosGoingFirst]: {},
+    [S.playing]: {
+      states: {
+        [S.playingTakingTurn]: {},
+        [S.playingCheckingGameEnd]: {},
+      },
+    },
+    [S.showingGameEndResults]: {},
+  },
+});
